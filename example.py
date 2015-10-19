042e102af94cdc76c58fec045082dfda60a1ef2d
daremon <daremon@gmail.com>
2015-10-19 15:59:33 +0300

    Changelog and example for Python3

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import random
 import urlclustering
 
 
 def pprint(clusters):
-    for key, urls in clusters.iteritems():
-        print 'REGEX:', key[0]
-        print 'HUMAN:', key[1]
-        print 'URLS:'
-        print '\t' + '\n\t'.join(urls) + '\n'
+    for key, urls in clusters.items():
+        print('REGEX:', key[0])
+        print('HUMAN:', key[1])
+        print('URLS:')
+        print('\t' + '\n\t'.join(urls) + '\n')
 
 urls = [
     u'http://example.com',
@@ -17,11 +18,11 @@ urls = [
 cats = [u'http://example.com/cat/%s' % x
         for x in ('sports', 'tech', 'life', 'politics', 'world')]
 tags = [u'http://example.com/tag/%s/tag%s' % (random.randint(100, 999), x)
-        for x in xrange(10)]
-arts = [u'http://example.com/article/?id=%s' % x for x in xrange(10)]
+        for x in range(10)]
+arts = [u'http://example.com/article/?id=%s' % x for x in range(10)]
 
 c = urlclustering.cluster(urls + cats + tags + arts, 5)
 
 pprint(c['clusters'])
-print 'UNCLUSTERED:'
-print '\t' + '\n\t'.join(c['unclustered'])
+print('UNCLUSTERED:')
+print('\t' + '\n\t'.join(c['unclustered']))
